91eb945b466895d8e9b6dae2e276fc0655b5bd7d
Jayanta Pradhan <pradhanjayanta91@gmail.com>
2026-02-06 00:52:19 +0530

    impliment basic version of client and server

---
+++ build/CMakeFiles/redis_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/redis_cpp.dir/src/main.cpp.o"
+  "CMakeFiles/redis_cpp.dir/src/main.cpp.o.d"
+  "redis_cpp"
+  "redis_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/redis_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
